a78036f0dc432d4f9d33c07fb1e7d0f6c6220e7b
linzhouzhi <490664646@qq.com>
2018-11-11 00:16:57 +0800

    tes

--- mobile/public/js/index.js
+++ mobile/public/js/index.js
@@ -1,11 +1,3 @@
 /**
  * Created by lzz on 2018/9/23.
  */
-
-$(function() {
-    $(".swiper-container").swiper({
-        autoplay:2000,
-        el: '.swiper-pagination',
-        dynamicBullets: true
-    });
-});
