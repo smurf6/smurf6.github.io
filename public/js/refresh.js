fb73c39898f8a19ab8b6be4b3ef5ad67f66d2320
linzhouzhi <490664646@qq.com>
2018-11-17 14:16:28 +0800

    refresh

--- public/js/refresh.js
+++ public/js/refresh.js
@@ -22,8 +22,7 @@ function refresh(id, position, callback){
 
     parent.on('touchmove',function (e) {
         var distance = Math.abs(e.originalEvent.targetTouches[0].pageY-startY);
-        if( distance > 50 ){
-            console.log(distance);
+        if( isTop() && distance > 50 ){
             ableLoad = true;
             refreshText.classList.remove("hidden");
         }else{
@@ -55,8 +54,47 @@ function refresh(id, position, callback){
     });
 
     function isTop(){
-        var t = document.documentElement.scrollTop||document.body.scrollTop;
-        return t === 0 ? true : false;
+        if(getScrollHeight() == getWindowHeight() + getDocumentTop()){
+            return true;
+        }
+        return false;
     }
 
 }
+
+//文档高度
+function getDocumentTop() {
+    var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
+    if (document.body) {
+        bodyScrollTop = document.body.scrollTop;
+    }
+    if (document.documentElement) {
+        documentScrollTop = document.documentElement.scrollTop;
+    }
+    scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
+    return scrollTop;
+}
+//可视窗口高度
+
+function getWindowHeight() {
+    var windowHeight = 0;
+    if (document.compatMode == "CSS1Compat") {
+        windowHeight = document.documentElement.clientHeight;
+    } else {
+        windowHeight = document.body.clientHeight;
+    }
+    return windowHeight;
+}
+//滚动条滚动高度
+function getScrollHeight() {
+    var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
+    if (document.body) {
+        bodyScrollHeight = document.body.scrollHeight;
+    }
+    if (document.documentElement) {
+        documentScrollHeight = document.documentElement.scrollHeight;
+    }
+    scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
+    return scrollHeight;
+}
+
