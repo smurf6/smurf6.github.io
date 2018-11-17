d10e38dfb2a0c7db0b3bcd206d418cfa92160fc3
linzhouzhi <490664646@qq.com>
2018-11-17 17:15:20 +0800

    test

--- public/js/index.js
+++ public/js/index.js
@@ -27,14 +27,31 @@ function initHome() {
                         slidesPerView: 3,//一行显示3个
                     });
 
-                    refresh("refreshContainer", "bottom", function () {
-                        window.guita.hotguita({}, function (data) {
-                            var hot = {"hot": data.res};
-                            console.log(hot);
-                            smarty.append("hot_guita", hot, "refreshContainer", function(){
+                    var miniRefresh = new MiniRefresh({
+                        container: '#minirefresh',
+                        down: {
+                            callback: function() {
+                                // 下拉事件
+                                setTimeout(function(){
+                                    miniRefresh.endDownLoading();
 
-                            });
-                        });
+                                },2000)
+
+                            }
+                        },
+                        up: {
+
+                            callback: function() {
+                                window.guita.hotguita({}, function (data) {
+                                    var hot = {"hot": data.res};
+                                    console.log(hot);
+                                    smarty.append("hot_guita", hot, "hot-guita", function(){
+
+                                    });
+                                });
+                                miniRefresh.endUpLoading(false);
+                            }
+                        }
                     });
                 });
             });
--- public/js/refresh.js
+++ public/js/refresh.js
@@ -21,8 +21,8 @@ function refresh(id, position, callback){
     //4.给列表dom监听touchmove事件，当移动到一定程度需要显示上面的文字
 
     parent.on('touchmove',function (e) {
-        var distance = Math.abs(e.originalEvent.targetTouches[0].pageY-startY);
-        if( isTop() && distance > 50 ){
+        var distance = e.originalEvent.targetTouches[0].pageY-startY;
+        if( isTop() && distance < -50 ){
             ableLoad = true;
             refreshText.classList.remove("hidden");
         }else{
@@ -54,47 +54,10 @@ function refresh(id, position, callback){
     });
 
     function isTop(){
-        if(getScrollHeight() == getWindowHeight() + getDocumentTop()){
-            return true;
-        }
-        return false;
+        console.log( $(window).scrollTop());
+        console.log(  $(window).height() );
+        console.log( $("#page").height());
+        return true;
     }
 
 }
-
-//文档高度
-function getDocumentTop() {
-    var scrollTop = 0, bodyScrollTop = 0, documentScrollTop = 0;
-    if (document.body) {
-        bodyScrollTop = document.body.scrollTop;
-    }
-    if (document.documentElement) {
-        documentScrollTop = document.documentElement.scrollTop;
-    }
-    scrollTop = (bodyScrollTop - documentScrollTop > 0) ? bodyScrollTop : documentScrollTop;
-    return scrollTop;
-}
-//可视窗口高度
-
-function getWindowHeight() {
-    var windowHeight = 0;
-    if (document.compatMode == "CSS1Compat") {
-        windowHeight = document.documentElement.clientHeight;
-    } else {
-        windowHeight = document.body.clientHeight;
-    }
-    return windowHeight;
-}
-//滚动条滚动高度
-function getScrollHeight() {
-    var scrollHeight = 0, bodyScrollHeight = 0, documentScrollHeight = 0;
-    if (document.body) {
-        bodyScrollHeight = document.body.scrollHeight;
-    }
-    if (document.documentElement) {
-        documentScrollHeight = document.documentElement.scrollHeight;
-    }
-    scrollHeight = (bodyScrollHeight - documentScrollHeight > 0) ? bodyScrollHeight : documentScrollHeight;
-    return scrollHeight;
-}
-
