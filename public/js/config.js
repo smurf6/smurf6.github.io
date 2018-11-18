1ae1575f2e5e38612e0634ff98701af9cd7f6bbb
linzhouzhi <490664646@qq.com>
2018-11-18 22:42:48 +0800

    config

--- public/js/config.js
+++ public/js/config.js
@@ -1,2 +1,3 @@
 //window.apiurl="http://127.0.0.1:8182/super/api/restful/";
-window.apiurl="http://39.105.114.48:8182/super/api/restful/";
+window.domain="http://39.105.114.48:8182/";
+window.apiurl=window.domain + "super/api/restful/";
