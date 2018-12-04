a2c599fc8de4d2f6eb459a2980cd0bb73d0ca54d
linzhouzhi <490664646@qq.com>
2018-12-04 22:26:35 +0800

    js

--- public/js/video.js
+++ public/js/video.js
@@ -2,11 +2,18 @@
  * Created by lzz on 2018/11/11.
  */
 
+window.videoPageCount = 0;
 function videoHtml() {
+    window.videoPageCount++;
     smarty.html("video",{}, "page", function(){
         window.video.videoList({"pageindex":0,"pageSize":2}, function (data) {
+            data.pageCount = window.videoPageCount;
             smarty.html("video_item", data, "video-list", function(){
-
+                var arr = data.res;
+                for(var i = 0; i < arr.length; i++){
+                    var id = "video-" + arr[i]["id"] + "-" + window.videoPageCount;
+                    videojs(id);
+                }
                 refresh("minirefresh",null, function () {
                     var pageSize = 1;
                     var pageIndex = $("#video-list").children(".card").length;
@@ -21,8 +28,13 @@ function videoHtml() {
                             return;
                         }else{
                             window.miniRefresh.endUpLoading(false);
+                            data.pageCount = window.videoPageCount;
                             smarty.append("video_item", data, "video-list", function(){
-
+                                var arr = data.res;
+                                for(var i = 0; i < arr.length; i++){
+                                    var id = "video-" + arr[i]["id"] + "-" + window.videoPageCount;
+                                    videojs(id);
+                                }
                             });
                         }
                     });
