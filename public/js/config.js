3e4898d2731ee498fef97ecd689255df012512ad
linzhouzhi <490664646@qq.com>
2018-12-05 23:25:09 +0800

    config

--- public/js/config.js
+++ public/js/config.js
@@ -2,22 +2,7 @@ window.domain="http://39.105.114.48:8182/";
 //window.domain="http://localhost:8182/"
 window.apiurl=window.domain + "super/api/restful/";
 
-if (window != top) {
-    console.log("000");
-}
-if (self.frameElement) {
-    console.log("111");
-}
-//方式二
-if (parent.frames.length) {
-    console.log("222");
-}
-//方式三
-if (self != top) {
-    console.log("333");
-}
-
-if(self.frameElement){
+if(window != top){
     console.log("开始执行下面");
     initAdminJs();
 }
