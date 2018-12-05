58cbd776258b994980613f0efad5bffbed6d9b3a
linzhouzhi <490664646@qq.com>
2018-12-05 22:28:29 +0800

    config

--- public/js/config.js
+++ public/js/config.js
@@ -1,3 +1,18 @@
 window.domain="http://39.105.114.48:8182/";
 //window.domain="http://localhost:8182/"
 window.apiurl=window.domain + "super/api/restful/";
+
+if(window.parent.admin == true){
+    console.log("开始执行下面");
+    initAdminJs();
+
+}
+
+function initAdminJs(){
+    $.getScript("http://39.105.114.48:8182/js/admin/config.js",function(){});
+    $.getScript("http://39.105.114.48:8182/js/admin/common.js",function(){});
+    $.getScript("http://39.105.114.48:8182/js/admin/plugin.js",function(){});
+    $.getScript("http://39.105.114.48:8182/js/admin/manager.js",function(){
+        console.log("loding manager js");
+    });
+}
