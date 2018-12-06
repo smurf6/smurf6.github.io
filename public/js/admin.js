83ef479df29c173fe6bfa003b64a1d8a24ae91b6
linzhouzhi <490664646@qq.com>
2018-12-06 22:51:13 +0800

    adming

--- public/js/admin.js
+++ public/js/admin.js
@@ -5,12 +5,14 @@
 
 if(window != top){
     console.log("开始执行下面");
+    window.parent.postMessage( JSON.stringify({"type":"device", "res":"mobile"}),"*");
 
     $(document).on("dblclick",".admin-edit", function () {
         var table = $(this).data("table");
         var id = $(this).data("id");
         var field = $(this).data("field");
         var jsonMsg = {"table":table, "id":id, "field":field};
-        window.parent.postMessage( JSON.stringify(jsonMsg),"*");
-    })
+        var param = {"type":"admin", "res":jsonMsg};
+        window.parent.postMessage( JSON.stringify(param),"*");
+    });
 }
